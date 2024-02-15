a9a2fd96c74d903ae9e95a1b26b407b27f65713c
Robin Lillqvist <robin.lillqvist@knowit.se>
2024-02-15 14:25:13 +0100

    implement search

--- Controllers/SearchPageController.cs
+++ Controllers/SearchPageController.cs
@@ -1,11 +1,19 @@
 using AlloyDemo.Models.Pages;
 using AlloyDemo.Models.ViewModels;
+using EPiServer.Find;
+using EPiServer.Find.UnifiedSearch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AlloyDemo.Controllers;
 
 public class SearchPageController : PageControllerBase<SearchPage>
 {
+    private readonly IClient searchClient;
+    public SearchPageController(IClient searchClient)
+    {
+        this.searchClient = searchClient;
+    }
+
     public ViewResult Index(SearchPage currentPage, string q)
     {
         var model = new SearchContentModel(currentPage)
@@ -15,6 +23,19 @@ public class SearchPageController : PageControllerBase<SearchPage>
             SearchServiceDisabled = true,
             SearchedQuery = q
         };
+
+        if(!string.IsNullOrWhiteSpace(q)) {
+            UnifiedSearchResults results = searchClient.UnifiedSearchFor(q).GetResult();
+
+            model.Hits = results.Hits.Select(hit =>
+            new SearchContentModel.SearchHit
+            {
+                Title = hit.Document.Title,
+                Url = hit.Document.Url,
+                Excerpt = hit.Document.Excerpt,
+            });
+            model.NumberOfHits = results.TotalMatching;
+        }
 
         return View(model);
     }
---
+++ Models/ViewModels/SearchPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using AlloyDemo.Models.Pages;
+using System.Collections.Generic;
+
+namespace AlloyDemo.Models.ViewModels
+{
+    public class SearchPageViewModel : PageViewModel<SearchPage>
+    {
+        public SearchPageViewModel(SearchPage currentPage) : base(currentPage){
+        }
+        public string SearchText { get; set; }
+        public List<Result> SearchResults { get; set; }
+    }
+    public class Result
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+    }
+}
