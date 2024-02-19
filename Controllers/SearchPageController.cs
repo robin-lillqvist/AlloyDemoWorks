038e4fd54d04f853df779dca8e96bdd9ef5ae70c
Robin Lillqvist <robin.lillqvist@knowit.se>
2024-02-19 09:16:48 +0100

    Added form

--- Controllers/SearchPageController.cs
+++ Controllers/SearchPageController.cs
@@ -2,41 +2,42 @@ using AlloyDemo.Models.Pages;
 using AlloyDemo.Models.ViewModels;
 using EPiServer.Find;
 using EPiServer.Find.UnifiedSearch;
-using EPiServer.Shell.ViewComposition;
-using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Mvc;
-namespace AlloyDemo.Controllers
+
+namespace AlloyDemo.Controllers;
+
+public class SearchPageController : PageControllerBase<SearchPage>
 {
-    public class SearchPageController : PageControllerBase<SearchPage>
+    private readonly IClient searchClient;
+    public SearchPageController(IClient searchClient)
     {
-        private readonly IClient searchClient;
-        public SearchPageController(IClient searchClient)
+        this.searchClient = searchClient;
+    }
+
+    public ViewResult Index(SearchPage currentPage, string q)
+    {
+        var model = new SearchContentModel(currentPage)
         {
-            this.searchClient = searchClient;
-        }
-        public ViewResult Index(SearchPage currentPage, string q)
+            Hits = Enumerable.Empty<SearchContentModel.SearchHit>(),
+            NumberOfHits = 0,
+            SearchServiceDisabled = false,
+            SearchedQuery = q
+        };
+
+        if (!string.IsNullOrWhiteSpace(q))
         {
-            var model = new SearchContentModel(currentPage)
+            UnifiedSearchResults results = searchClient.UnifiedSearchFor(q).GetResult();
+
+            model.Hits = results.Hits.Select(hit =>
+            new SearchContentModel.SearchHit
             {
-                Hits = Enumerable.Empty<SearchContentModel.SearchHit>(),
-                NumberOfHits = 0,
-                SearchServiceDisabled = false,
-                SearchedQuery = q
-            };
-            if (!string.IsNullOrWhiteSpace(q))
-            {
-                UnifiedSearchResults results = searchClient
-                .UnifiedSearchFor(q).GetResult();
-                model.Hits = results.Hits.Select(hit =>
-                new SearchContentModel.SearchHit()
-                {
-                    Title = hit.Document.Title,
-                    Url = hit.Document.Url,
-                    Excerpt = hit.Document.Excerpt
-                });
-                model.NumberOfHits = results.TotalMatching;
-            }
-            return View(model);
+                Title = hit.Document.Title,
+                Url = hit.Document.Url,
+                Excerpt = hit.Document.Excerpt,
+            });
+            model.NumberOfHits = results.TotalMatching;
         }
+
+        return View(model);
     }
 }
